f9ab1bc685558bb95844d98943674cd8e13d1318
cransom17 <143670310+cransom17@users.noreply.github.com>
2024-01-27 22:16:41 -0500

    mmm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function HelloWorld(){
+  return<h1 className="greeting"> "Hello World!"</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +13,6 @@ root.render(
     <HelloWorld />
   </React.StrictMode>
 );
-function HelloWorld(){
-  return<h1 className="greeting"> "Hello World!"</h1>;
-}
 
 
 // If you want to start measuring performance in your app, pass a function
