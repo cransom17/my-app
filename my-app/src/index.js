74ff05a08b88a63fc46b31c4d194afaccad02e16
cransom17 <143670310+cransom17@users.noreply.github.com>
2024-01-27 22:04:14 -0500

    starting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,13 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <App />
   </React.StrictMode>
 );
-function HelloWorld(){
-  return<h1 className="greeting"> "Hello World!"</h1>;
-}
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
